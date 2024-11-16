c0b4efdc6af18d6450c85dd7a4026c0912a57075
kashifansaricodes <kansari@umd.edu>
2024-11-15 23:09:33 -0500

    adding extras

--- include/simple_publisher.hpp
+++ include/simple_publisher.hpp
@@ -6,6 +6,13 @@
  * @date 2024-02-08
  *
  * @copyright Copyright (c) 2024 Kashif Ansari
+ * @license MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software.
  *
  * @details This header defines a ROS2 publisher node that publishes string
  * messages with configurable frequency and provides a service to modify the
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,14 @@
  * @version 0.1
  * @date 2024-02-08
  * 
- * @copyright Copyright (c) 2024
+ * @copyright Copyright (c) 2024 Kashif Ansari
+ * @license MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software.
  */
 
 #include "simple_publisher.hpp"
